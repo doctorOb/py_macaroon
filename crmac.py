dfc5a90441b68705e080b51e027b9513b21f9e2b
Dan Robertson <drob.drobertson@gmail.com>
2014-04-12 20:13:23 -0400

    cleaned up code a bit, login now works

--- crmac.py
+++ crmac.py
@@ -11,41 +11,35 @@ def old_post_login():
 
 	req = urllib2.Request(login_url, data, )
 
-def post_login():
-	s = requests.Session()
-	data = {  'sid' : 'drob',
-			  'PIN' : 'yuBfx2020'}
-	response = s.get(login_url,
-		headers={'User-Agent' : 'curl/7.30.0',
-				 'Host' : 'bannerweb.wpi.edu',
-				 'Accept' : '*/*'}
+def _headers():
+	"""return a default list of headers for use in all requests. Some / all of these might not be necessary"""
+	return {
+		'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36', #pretend I'm a browser
+		'Host' : 'bannerweb.wpi.edu', 
+		'Accept' : '*/*'
+	}
+
+
+def post_login(s=None):
+	s = requests.session() if s is None else s
+	def_headers = _headers()
+	r1 = s.get(login_url,
+		headers=def_headers
 	)
 
-	cookies = response.cookies
+	if int(r1.status_code) == 403:
+		return None
 
-	response = s.post(login_url, 
-		data=data, 
-		headers={'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'},
-		cookies=cookies
+	r2 = s.post(login_url, 
+		data={  'sid' : 'drob', #username
+				'PIN' : ''}, #password 
+		headers=def_headers,
 	)
-	#print response.text.encode('ascii','ignore')
-	print response.status_code
-	print response.headers
+
+	if int(r2.status_code) == 403:
+		return None
+
 	return s
 
-def br_em():
-	br = mechanize.Browser()
-	br.open("https://bannerweb.wpi.edu")
-	br.select_form(name="loginform")
-	br["sid"] = "drob"
-	br["PIN"] = "yuBfx2020"
-	resp = br.submit()
-	print resp.info()
-	resp = br.open(registration_url)
-	#print resp.read()
-	br["term_in"] = "201502"
-	resp = br.submit()
-	print resp.read()
-
 if __name__ == '__main__':
-	post_login()
+	session = post_login()
