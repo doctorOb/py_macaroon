c0e0b0c8e9d985f515c584784af29662f94373ca
Ryan Baker <ryan.da.baker@gmail.com>
2014-04-12 20:09:04 -0400

    added read file capabilities and a gitignore

--- crmac.py
+++ crmac.py
@@ -13,8 +13,10 @@ def old_post_login():
 
 def post_login():
 	s = requests.Session()
-	data = {  'sid' : 'drob',
-			  'PIN' : 'yuBfx2020'}
+    f = str(raw_input("What is the name of the file with your WPI username and password?\n"))
+    creds_file = open(f,'r')
+    data['sid'] = creds_file.readline()[:-1] # ignore newline
+    data['PIN'] = creds_file.readline()[:-1] # ignore newline
 	response = s.get(login_url,
 		headers={'User-Agent' : 'curl/7.30.0',
 				 'Host' : 'bannerweb.wpi.edu',
@@ -32,20 +34,6 @@ def post_login():
 	print response.status_code
 	print response.headers
 	return s
-
-def br_em():
-	br = mechanize.Browser()
-	br.open("https://bannerweb.wpi.edu")
-	br.select_form(name="loginform")
-	br["sid"] = "drob"
-	br["PIN"] = "yuBfx2020"
-	resp = br.submit()
-	print resp.info()
-	resp = br.open(registration_url)
-	#print resp.read()
-	br["term_in"] = "201502"
-	resp = br.submit()
-	print resp.read()
 
 if __name__ == '__main__':
 	post_login()
